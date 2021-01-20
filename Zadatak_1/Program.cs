8d0084f1202d3aeaefa1373cf3832e08c04914dc
Nikola <nstipic@etfos.hr>
2021-01-20 18:08:31 +0100

    Prazna apk

---
+++ Zadatak_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zadatak_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
